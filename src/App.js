0287953c46519cd55e2eec77d810afb9b10f3886
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-06 05:38:02 +0530

    initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+    <div>
+      <Navbar/>
+    </div>
+      <Routes>
+        <Route path='/' element={<Home/>} exact/>
+      </Routes>
+    </BrowserRouter>
+    <div>
+      <Footer/>
+    </div>
     </div>
   );
 }
